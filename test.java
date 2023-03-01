f6ea39f3e7fbb919fdf72a54e8ee175b3b09a4e6
hwewon <126555995+hwewon@users.noreply.github.com>
2023-03-01 16:25:17 +0900

    Create test.java

---
+++ test.java
@@ -0,0 +1,11 @@
+public class git_practice
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		System.out.println("안녕하세요");
+	}
+
+}
